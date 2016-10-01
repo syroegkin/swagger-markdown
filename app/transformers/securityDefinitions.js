11946eb8a9faeb38b762ccf71e1a0be529e48bed
Sergii <syroegkin@gmail.com>
2016-10-01 11:39:45 +0200

    Security definition transformer

---
+++ app/transformers/securityDefinitions.js
@@ -0,0 +1,14 @@
+module.exports = securityDefinitions => {
+  // Base block
+  const res = [];
+  // securityDefinitions.map(auth => {
+  //
+  // });
+
+  // Create header
+  // Only in case if there is some data
+  if (res.length > 0) {
+    res.push('### Security');
+    res.push('---');
+  }
+};
