41917a5a622fa1e66c5b5b359036cddde0a40171
grdlok <adam@hello-world.ca>
2017-11-23 23:06:28 -0500

    Add primitive definitions

--- app/transformers/definitions.js
+++ app/transformers/definitions.js
@@ -3,20 +3,13 @@ const inArray = require('../lib/inArray');
 const Schema = require('../models/schema');
 
 /**
- * @param {type} name
- * @param {type} definition
- * @return {type} Description
+ * If Property field is present parse them.
+ * @param name of the definition
+ * @param definition definition object
  */
-const processDefinition = (name, definition) => {
-  const res = [];
+const parseProperties = (name, definition) => {
   const required = 'required' in definition ? definition.required : [];
-
-  // Add anchor with name
-  res.push('');
-  res.push(`### ${name}  `);
-  res.push('');
-  res.push('| Name | Type | Description | Required |');
-  res.push('| ---- | ---- | ----------- | -------- |');
+  const res = [];
   Object.keys(definition.properties).map(propName => {
     const prop = definition.properties[propName];
     const typeCell = dataTypeTransformer(new Schema(prop));
@@ -24,11 +17,47 @@ const processDefinition = (name, definition) => {
     const requiredCell = inArray(propName, required) ? 'Yes' : 'No';
     res.push(`| ${propName} | ${typeCell} | ${descriptionCell} | ${requiredCell} |`);
   });
+  return res;
+};
+
+/**
+ * Parse allOf defintion
+ * @param name of the definition
+ * @param definition definition object
+ */
+const parsePrimitive = (name, definition) => {
+  const res = [];
+  const typeCell = 'type' in definition ? definition.type : '';
+  const descriptionCell = 'description' in definition ? definition.description : '';
+  const requiredCell = 'required' in definition ? definition.required : '';
+  res.push(`| ${name} | ${typeCell} | ${descriptionCell} | ${requiredCell} |`);
+  return res;
+};
+
+/**
+ * @param {type} name
+ * @param {type} definition
+ * @return {type} Description
+ */
+const processDefinition = (name, definition) => {
+  let res = [];
+  let parsedDef = [];
+  res.push('');
+  res.push(`### ${name}  `);
+  res.push('');
+  res.push('| Name | Type | Description | Required |');
+  res.push('| ---- | ---- | ----------- | -------- |');
+
+  if ('properties' in definition) {
+    parsedDef = parseProperties(name, definition);
+  } else {
+    parsedDef = parsePrimitive(name, definition);
+  }
+  res = res.concat(parsedDef);
 
   return res.length ? res.join('\n') : null;
 };
 module.exports.processDefinition = processDefinition;
-
 
 /**
  * @param {type} definitions
