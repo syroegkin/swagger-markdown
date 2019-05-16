ad72a3bb919ae028db1b726abb4aef61875a2d49
Florian Keller <github@floriankeller.de>
2019-05-16 14:47:07 +0200

    fix(definitions): Replace newlines with spaces in table

--- app/transformers/pathParameters.js
+++ app/transformers/pathParameters.js
@@ -8,37 +8,37 @@ module.exports = (parameters, pathParameters, globalParameters = {}) => {
   res.push('| ---- | ---------- | ----------- | -------- | ---- |');
   [].concat(pathParameters, parameters).map(keys => {
     if (keys) {
-      let paramsetersKeys = keys;
+      let parametersKeys = keys;
       // Check it for the reference
       if ('$ref' in keys) {
         const ref = (keys.$ref).replace(/^#\/parameters\//, '');
         if (ref in globalParameters) {
-          paramsetersKeys = globalParameters[ref];
+          parametersKeys = globalParameters[ref];
         }
       }
       const line = [];
       // Name first
-      line.push(paramsetersKeys.name || '');
+      line.push(parametersKeys.name || '');
       // Scope (in)
-      line.push(paramsetersKeys.in || '');
+      line.push(parametersKeys.in || '');
       // description
-      if ('description' in paramsetersKeys) {
-        line.push(paramsetersKeys.description.replace(/[\r\n]/g, ' '));
+      if ('description' in parametersKeys) {
+        line.push(parametersKeys.description.replace(/[\r\n]/g, ' '));
       } else {
         line.push('');
       }
-      line.push(paramsetersKeys.required ? 'Yes' : 'No');
+      line.push(parametersKeys.required ? 'Yes' : 'No');
 
       // Prepare schema to be transformed
       let schema = null;
-      if ('schema' in paramsetersKeys) {
-        schema = new Schema(paramsetersKeys.schema);
+      if ('schema' in parametersKeys) {
+        schema = new Schema(parametersKeys.schema);
       } else {
         schema = new Schema();
-        schema.setType('type' in paramsetersKeys ? paramsetersKeys.type : null);
-        schema.setFormat('format' in paramsetersKeys ? paramsetersKeys.format : null);
-        schema.setReference('$ref' in paramsetersKeys ? paramsetersKeys.$ref : null);
-        schema.setItems('items' in paramsetersKeys ? paramsetersKeys.items : null);
+        schema.setType('type' in parametersKeys ? parametersKeys.type : null);
+        schema.setFormat('format' in parametersKeys ? parametersKeys.format : null);
+        schema.setReference('$ref' in parametersKeys ? parametersKeys.$ref : null);
+        schema.setItems('items' in parametersKeys ? parametersKeys.items : null);
       }
 
       line.push(transformDataTypes(schema));
