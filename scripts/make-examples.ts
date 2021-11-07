856f4d4dbc0b259a2151bc012279e2eaae84fd37
Sergio <syroegkin@protonmail.com>
2021-11-07 22:10:54 +0100

    feat: make examples to typescript

--- scripts/make-examples.ts
+++ scripts/make-examples.ts
@@ -1,17 +1,18 @@
-const { execSync } = require('child_process');
-const path = require('path');
-const fs = require('fs');
+import path from 'path';
+import fs from 'fs';
+import { execSync } from 'child_process';
 
 const directory = path.join(__dirname, '..', 'examples');
+
 fs.readdir(directory, (err, files) => {
   if (err) {
-    console.log(`Unable to read directory: ${err}`);
+    console.error(`Unable to read directory: ${err}`);
     return;
   }
   files.forEach((filename) => {
     if (!filename.match(/\.yaml$/)) return;
     console.log(`Processing ${filename}`);
-    execSync(`node src/index.js -i examples/${filename}`);
+    execSync(`node dist/index.js -i examples/${filename}`);
     console.log('Done\n');
   });
 });
