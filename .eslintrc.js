3985cda3143d60c24c0f72a125729dd7ab032086
Sergio <syroegkin@protonmail.com>
2022-05-21 15:49:20 +0200

    style(eslint): fix ts related indent problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,8 @@ module.exports = {
     '@typescript-eslint/no-shadow': ['error'],
     'no-useless-constructor': 0,
     '@typescript-eslint/no-useless-constructor': ['error'],
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2],
   },
   overrides: [
     {
